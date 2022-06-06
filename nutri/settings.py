ea70e67a341856e23c27355b5bfd9f52c8527e19
vikram negi <vikramnegi175@gmail.com>
2022-06-06 22:36:26 +0530

    reuirements

--- nutri/settings.py
+++ nutri/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static_cdn')
-# ]
+# STATICFILES_DIRS = [BASE_DIR/'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
